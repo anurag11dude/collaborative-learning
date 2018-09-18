7642e5f845706a710528e4a1464bb7c165db9855
dalove1025 <dalove1025@gmail.com>
2018-09-18 11:33:37 -0400

    WIP - comment to capture our thinking

--- src/components/canvas-tools/text-tool.tsx
+++ src/components/canvas-tools/text-tool.tsx
@@ -17,28 +17,68 @@ interface IProps {
   readOnly?: boolean;
 }
 
+/*
+ * This comment to be removed once we're done with development of the
+ * initial clipboard functionality. For our first work we have some working
+ * definitions and assumptions. The assumptions are numbered.
+ * 
+ * 1. For now, we are not concerned with drag-and-drop. Only copy, paste, and
+ * at the tool-level, delete. (If desired, cut is always something that can
+ * be composed by a copy-delete.)
+ * 
+ * Selection can be at either the tool-level or at a primitive level, inside 
+ * a tool. For example, one might click on an area of the page that contains
+ * a text-tool. If so, that particular text-tool will be selected. Previously
+ * selected tools are unselected. We talk about primitive selections, later.
+ * 
+ * 2. When a tool is selected, (at the moment, the only tool is the text-tool)
+ * we visually indicate it is selected, by rendering with a pink background.
+ * This rendering will be finalized when we know exactly how we want it to look.
+ * 
+ * A tool maybe be in various clipboard-related selection states -- it may be:
+ * 
+ *    *  not selected,
+ *    *  singly selected, or
+ *    *  one (of two or more) selected tools.
+ * 
+ * 3. For now, we only support single selection which means at any one time,
+ * either no tool or only 1 tool may be selected -- in other words there will
+ * either be 1 tool with a pink background; or none.
+ * 
+ * A parallel notion in this model is the idea of the "active" tool. There
+ * can only be zero or 1 active tools at any one time.  The active tool is the
+ * tool that has the keyboard focus, or contains another sub-component that
+ * has the keyboard focus. In other words, it's the tool getting keyboard
+ * input and the copy and paste events. 
+ * 
+ * Tools in read-only canvas can only be selected, and may not be active. When
+ * a tool is selected in an read/write canvas, it is, for now, the only selected
+ * tool and is also the only active tool.
+ * 
+ * So, for now, we need not implement a list of selected tools and, instead
+ * implement the idea of a single "selectedTile". It needs to be owned by a
+ * container of the tool so it needed to be at the canvas, workspace, document,
+ * or ui level. Since we don't think the selection state is anything that would
+ * be seen by other users (say, in 4-up), nor saved in the database, we have
+ * wedged it into the ui model. If those assumptions change, this will likely
+ * mean this state (and other stuff) will need to be moved from the ui model
+ * and into a more apropos object.
+ *
+ * 4. Ergo, single selection is implemented in the ui model, in a property called
+ * "selectedTile" which is either the id of a toolTile that can be set with
+ * "setSelectedTile" or "clearSelectedTile". No selection is indidcated when
+ * the selectedTile is unknown.
+ * 
+ * 5. Selection behavior is implemented like this:
+ * 
+ *    * At launch, nothing is selected.
+ *    * Any shift of focus will cause the tool's tile to become selected.
+ *
+ */
+
 @inject("stores")
 @observer
 export default class TextToolComponent extends BaseComponent<IProps, IState> {
-
-  public onChange = (change: Change) => {
-    const { readOnly, model: { content } } = this.props;
-    const { ui } = this.stores;
-    // console.log(change.operations.get(0).type);
-    if (change.operations.get(0).type === "set_selection") {
-      ui.setSelectedTile(this.props.model);
-    }
-    if (content.type === "Text") {
-      if (readOnly) {
-        this.setState({
-          value: change.value
-        });
-      }
-      else {
-        content.setSlate(change.value);
-      }
-    }
-  }
 
   public render() {
     const { model, readOnly } = this.props;
@@ -60,6 +100,26 @@ export default class TextToolComponent extends BaseComponent<IProps, IState> {
         onCopy={this.onCopy}
       />
     );
+  }
+
+  private onChange = (change: Change) => {
+    const { readOnly, model: { content } } = this.props;
+    const { ui } = this.stores;
+    const op = change.operations.get(0);
+    //console.log('In TextToolComponent: ' + op.type);
+    if (change.operations.get(0).type === "set_selection") {
+      ui.setSelectedTile(this.props.model);
+    }
+    if (content.type === "Text") {
+      if (readOnly) {
+        this.setState({
+          value: change.value
+        });
+      }
+      else {
+        content.setSlate(change.value);
+      }
+    }
   }
 
   private onCopy = (e: Event, change: Change) => {
